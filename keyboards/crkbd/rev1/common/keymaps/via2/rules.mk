44845963b166eb7af39b207bf77e876cca5a30fe
Daniel <d_sanchez@ar3d.net>
2021-02-28 14:21:51 -0600

    Added custom crkbd keymap based on via

---
+++ keyboards/crkbd/rev1/common/keymaps/via2/rules.mk
@@ -0,0 +1,6 @@
+MOUSEKEY_ENABLE     = no     # Mouse keys
+RGBLIGHT_ENABLE     = no    # Enable WS2812 RGB underlight.
+VIA_ENABLE          = yes         # Enable VIA
+OLED_DRIVER_ENABLE  = yes
+LTO_ENABLE          = yes
+EXTRAKEY_ENABLE 	= yes
